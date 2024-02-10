d322104da721215dd2a735d63f899f108b9ce5e8
Maksim459 <159301480+Maksim459@users.noreply.github.com>
2024-02-10 18:46:58 +0300

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 0;
+        while (a <= 10) 
+       {
+            Task.Delay(100).Wait();
+            Console.Clear();
+            Console.WriteLine("  /\n /\n/");
+            
+            Task.Delay(100).Wait();
+            Console.Clear();
+            Console.WriteLine("\\\n \\\n  \\");
+            Task.Delay(100).Wait();
+            Console.Clear();
+            Console.Clear();
+           
+            a++;
+       }
+
+          
+       
+
+
+
+    } 
+
+       
+   
+}
